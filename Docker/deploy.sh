3864f165d35ec9b6b7c90379c7996b707494504b
root <root@ip-172-31-33-82.ap-south-1.compute.internal>
2020-09-04 09:41:35 +0000

    Updated at 3-11

--- Docker/deploy.sh
+++ Docker/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-revision=$(aws ecs describe-task-definition --task-definition jenkins-ecs | egrep "revision" | tr "," " " | awk '{print $2}' | sed 's/"$//')
+revision=$(aws ecs describe-task-definition --task-definition new-jenkins | egrep "revision" | tr "," " " | awk '{print $2}' | sed 's/"$//')
 echo $revision
-aws ecs update-service --cluster jenkins-demo --service temp-service --task-definition "jenkins-ecs":"$revision"
+aws ecs update-service --cluster new-jenkins --service temp-service --task-definition "jenkinsonforth":"$revision"
